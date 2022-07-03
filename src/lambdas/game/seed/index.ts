24117d5005c8bc87dbfbdc9bbe148f4d014f39ea
Juneil <fauville.j@gmail.com>
2022-07-03 15:56:36 +0200

    refacto: clean

--- src/lambdas/game/seed/index.ts
+++ src/lambdas/game/seed/index.ts
@@ -1,25 +1,25 @@
-import { APIGatewayProxyResponse, Cors, generateHandler, Lambda, Logger, PathParams, Payload } from '@ekonoo/lambdi';
-import { GetPathParam, PostAppendSeed } from '../../../models/api/game.model';
-import { GameService } from '../../../services/game.service';
-import { BusinessErrorResponse } from '../../../utils/error';
-import { createErrorResponse, createResponse } from '../../../utils/response';
+// import { APIGatewayProxyResponse, Cors, generateHandler, Lambda, Logger, PathParams, Payload } from '@ekonoo/lambdi';
+// import { GetPathParam, PostAppendSeed } from '../../../models/api/game.model';
+// import { GameService } from '../../../services/game.service';
+// import { BusinessErrorResponse } from '../../../utils/error';
+// import { createErrorResponse, createResponse } from '../../../utils/response';
 
-@Lambda({
-    providers: [GameService]
-})
-export class GameAppendSeedLambda {
-    constructor(private game: GameService, private readonly logger: Logger) {}
+// @Lambda({
+//     providers: [GameService]
+// })
+// export class GameAppendSeedLambda {
+//     constructor(private game: GameService, private readonly logger: Logger) {}
 
-    @Cors('*')
-    async onHandler(
-        @PathParams path: GetPathParam,
-        @Payload payload: PostAppendSeed
-    ): Promise<APIGatewayProxyResponse<void | BusinessErrorResponse>> {
-        return this.game
-            .updateStateSeed(path.id, payload.answers)
-            .then(res => createResponse(res))
-            .catch(err => createErrorResponse(err, this.logger));
-    }
-}
+//     @Cors('*')
+//     async onHandler(
+//         @PathParams path: GetPathParam,
+//         @Payload payload: PostAppendSeed
+//     ): Promise<APIGatewayProxyResponse<void | BusinessErrorResponse>> {
+//         return this.game
+//             .updateStateSeed(path.id, payload.answers)
+//             .then(res => createResponse(res))
+//             .catch(err => createErrorResponse(err, this.logger));
+//     }
+// }
 
-export const handler = generateHandler(GameAppendSeedLambda);
+// export const handler = generateHandler(GameAppendSeedLambda);
